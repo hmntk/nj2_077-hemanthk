4bf162a3470e0032a756cf010c946b4a60cd98b0
hmntk <hemanth.anits.85@gmail.com>
2021-08-04 03:59:17 +0530

    3rd Aug Assgn

--- Unit3-Redo/sprint2/day2/as/as3/Components/latest.js
+++ Unit3-Redo/sprint2/day2/as/as3/Components/latest.js
@@ -1,3 +1,88 @@
 import Navbar from "./navbar.js";
 
 document.getElementById("navbar").innerHTML = Navbar();
+
+async function Random() {
+  console.log(1);
+  let res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/categories.php`
+  );
+  //   let res = await fetch(
+  //     "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"
+  //   );
+  let { categories } = await res.json();
+
+  categories.forEach(
+    ({ idCategory, strCategoryThumb, strCategoryDescription }) => {
+      console.log(idCategory, strCategoryThumb, strCategoryDescription);
+
+      let div = document.createElement("div");
+      div.setAttribute("id", "item");
+      div.innerHTML = `
+      <div id="left">
+        <span>${idCategory}</span>
+        <p>${strCategoryDescription}
+        </p>
+      </div>
+      <div id="right">
+        <img
+          src="${strCategoryThumb}"
+          alt=""
+        />
+      </div>
+    `;
+      document.getElementById("recipe").append(div);
+    }
+  );
+  /*document.getElementById("generate").innerHTML =
+    "Click here again to generate another Recipe";
+  document.getElementById("recipe").style.display = "none";
+  meals.forEach(
+    ({
+      strMeal,
+      strInstructions,
+      strMealThumb,
+      strIngredient1,
+      strMeasure1,
+      strIngredient2,
+      strMeasure2,
+      strIngredient3,
+      strMeasure3,
+      strIngredient4,
+      strMeasure4,
+      strIngredient5,
+      strMeasure5,
+    }) => {
+      console.log(strMeal);
+      document.getElementById("recipe").style.display = "block";
+
+      let div = document.createElement("div");
+      div.setAttribute("id", "item");
+      div.innerHTML = `<div id="left">
+      <div id="title">
+        <h1>${strMeal}</h1>
+        <div id="ingredients">
+          <h3>Items Required</h3>
+          <ul>
+            <li>${strIngredient1} - ${strMeasure1}</li>
+            <li>${strIngredient2} - ${strMeasure2}</li>
+            <li>${strIngredient3} - ${strMeasure3}</li>
+            <li>${strIngredient4} - ${strMeasure4}</li>
+            <li>${strIngredient5} - ${strMeasure5}</li>
+          </ul>
+        </div>
+      </div>
+      <p>${strInstructions}
+      </p>
+    </div>
+    <div id="right">
+      <img
+        src="${strMealThumb}"
+        alt=""
+      />
+    </div>`;
+      document.getElementById("recipe").append(div);
+    }
+  );*/
+}
+Random();
--- Unit3-Redo/sprint2/day2/as/as3/Components/navbar.js
+++ Unit3-Redo/sprint2/day2/as/as3/Components/navbar.js
@@ -2,7 +2,7 @@ let Navbar = () => {
   return `<nav>
         <span>FOODAPP!</span>
         <a href="search.html"> <button>Search for Recipes</button></a>
-        <a href="random.html"><button>Recipe of the day</button></a>
+        <a href="random.html"><button id="random">Recipe of the day</button></a>
         <a href="latest.html"><button>Latest Recipes</button></a>
       </nav>`;
 };
